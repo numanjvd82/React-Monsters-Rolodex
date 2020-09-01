e45fea7968b7dc5aa2a27aae32237577f0cc5b06
Numan Javed <nomanmani62@gmail.com>
2020-09-01 19:21:07 +0500

    Updates

---
+++ precache-manifest.a0362f176f322403b6f6569929242005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a23f2648c97513ca2dedc9b1ff149f3",
+    "url": "/React-Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "239354873943fbf14c5f",
+    "url": "/React-Monsters-Rolodex/static/css/main.9ac7b03a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/React-Monsters-Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Monsters-Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239354873943fbf14c5f",
+    "url": "/React-Monsters-Rolodex/static/js/main.7c043bd6.chunk.js"
+  },
+  {
+    "revision": "0c3ad1a0c617ba45a318",
+    "url": "/React-Monsters-Rolodex/static/js/runtime-main.90bd8544.js"
+  }
+]);
